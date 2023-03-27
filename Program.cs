a3b4bee28732759fb93898b672346d75b9e583e8
miguel Martinez <garcia.miguelangel1206@gmail.com>
2023-03-27 17:15:46 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Por favor, introduce tu nombre:");
+        string nombre = Console.ReadLine();
+        Console.WriteLine($"¡Hola, {nombre}!");
